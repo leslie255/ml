c946433cbad84e19192d079b6f5dbb18fbb18ace
Leslie Kerman <kerbal255@outlook.com>
2024-05-05 05:07:27 +0100

    Sigmoid as activation function

--- src/main.c
+++ src/main.c
@@ -3,25 +3,8 @@
 #include "da.h"
 
 const f32 training_data[] = {
-    0,  0 * 2,  //
-    1,  1 * 2,  //
-    2,  2 * 2,  //
-    3,  3 * 2,  //
-    4,  4 * 2,  //
-    5,  5 * 2,  //
-    6,  6 * 2,  //
-    7,  7 * 2,  //
-    8,  8 * 2,  //
-    9,  9 * 2,  //
-    10, 10 * 2, //
-    11, 11 * 2, //
-    12, 12 * 2, //
-    13, 13 * 2, //
-    14, 14 * 2, //
-    15, 15 * 2, //
-    16, 16 * 2, //
-    17, 17 * 2, //
-    18, 18 * 2, //
+    0, 1, //
+    1, 0, //
 };
 
 typedef struct NN {
@@ -29,14 +12,17 @@ typedef struct NN {
   f32 bias;
 } NN;
 
+f32 sigmoidf(f32 x) {
+  return 1 / (1 + expf(-x));
+}
+
 f32 forward(NN nn, f32 in) {
-  return nn.weight * in + nn.bias;
+  return sigmoidf(nn.weight * in + nn.bias);
 }
 
 /// `training_count` is number of items (not number of pairs!)
 /// Returns the loss.
 f32 train(NN *nn, const f32 *training_pairs, usize training_count, usize i, f32 rate) {
-  usize n = training_count / 2;
   assert(training_count % 2 == 0);
   f32 loss = 0;
   f32 dw = 0;
@@ -47,14 +33,15 @@ f32 train(NN *nn, const f32 *training_pairs, usize training_count, usize i, f32 
     f32 nn_out = forward(*nn, training_in);
     f32 diff = nn_out - training_out;
     loss += diff * diff;
-    dw += diff * training_in;
-    db += diff;
+    dw += diff * nn_out * (1 - nn_out) * training_in;
+    db += diff * nn_out * (1 - nn_out);
   }
+  usize n = training_count / 2;
   loss = loss / n;
   dw = dw / n * 2;
   db = db / n * 2;
-  if ((i % 100) == 0)
-    printf("%zu\tloss: %.04f\tw: %.04f\tdw: %.04f\tb: %.04f\tdb: %.04f\n", i, loss, nn->weight, dw, nn->bias, db);
+  if ((i % 1000) == 0)
+    printf("%zu\tloss: %.08f\tw: %.04f\tdw: %.04f\tb: %.04f\tdb: %.04f\n", i, loss, nn->weight, dw, nn->bias, db);
   nn->bias -= db * rate;
   nn->weight -= dw * rate;
   return loss;
@@ -78,13 +65,13 @@ NN neuron_random(f32 floor, f32 ceil) {
 }
 
 int main() {
-  srand(10);
-  NN nn = neuron_random(-5, 5);
+  srand(time(NULL));
+  NN nn = neuron_random(-1, 1);
   DBG_PRINTLN(nn.weight);
   DBG_PRINTLN(nn.bias);
-  usize training_rounds = 10*1000;
+  usize training_rounds = 100 * 1000;
   for (usize i = 0; i < training_rounds; ++i) {
-    train(&nn, training_data, ARR_LEN(training_data), i, 1e-3);
+    train(&nn, training_data, ARR_LEN(training_data), i, 1e-2);
   }
   printf("-------------------------\n");
   DBG_PRINTLN(nn.weight);
@@ -92,7 +79,7 @@ int main() {
   for (usize i = 0; i < ARR_LEN(training_data); i += 2) {
     f32 in = training_data[i];
     f32 out = forward(nn, in);
-    printf("%.0f => %.04f ~= %.0f\n", in, out, roundf(out));
+    printf("%.0f => %.04f\n", in, out);
   }
   return 0;
 }
